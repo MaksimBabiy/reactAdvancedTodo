d1f66971e698b2794afcfecc430813af1b4decc5
mksninja <kitavaurod@gmail.com>
2020-03-02 22:19:22 +0200

    Updates

---
+++ precache-manifest.708c6578403486e2198071e2083dc7f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82eeac0008bd6b7e6e406a2c71244d55",
+    "url": "/reactAdvancedTodo/index.html"
+  },
+  {
+    "revision": "47ee16782cb9edaa82bc",
+    "url": "/reactAdvancedTodo/static/css/main.1ede93b6.chunk.css"
+  },
+  {
+    "revision": "00ae0264835ce1647556",
+    "url": "/reactAdvancedTodo/static/js/2.8342f0b3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/reactAdvancedTodo/static/js/2.8342f0b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ee16782cb9edaa82bc",
+    "url": "/reactAdvancedTodo/static/js/main.a5787f40.chunk.js"
+  },
+  {
+    "revision": "13ae5abf20388f629819",
+    "url": "/reactAdvancedTodo/static/js/runtime-main.73e8e05c.js"
+  },
+  {
+    "revision": "04bce1bad57c42f2363ab204267bb4eb",
+    "url": "/reactAdvancedTodo/static/media/add.04bce1ba.svg"
+  },
+  {
+    "revision": "10c581b6e3a0bc8ce7ac0527a423c2ad",
+    "url": "/reactAdvancedTodo/static/media/close.10c581b6.svg"
+  },
+  {
+    "revision": "e6d3868a10f056f22a226b8cbe602bc9",
+    "url": "/reactAdvancedTodo/static/media/edit.e6d3868a.svg"
+  },
+  {
+    "revision": "95f30ee9e97d62b2666b1f9c48e00ae7",
+    "url": "/reactAdvancedTodo/static/media/remove.95f30ee9.svg"
+  }
+]);
